c48d3b8e2d9dadfab8259512fbe0ec5b5531d459
arquillos <jalvarez@datiobd.com>
2017-03-18 22:13:46 +0100

    Third Commit

---
+++ sparktest/src/main/java/sparktest/RDDTransformations.java
@@ -0,0 +1,119 @@
+package sparktest;
+
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.FlatMapFunction;
+
+import java.util.Iterator; 
+
+
+public class RDDTransformations {
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("RDD Transformations");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		sc.setLogLevel("WARN");
+
+		System.out.println();
+		System.out.println("----------------------");
+		System.out.println("--- Filter");
+		JavaRDD<String> lines = sc.textFile("/var/log/messages");
+		
+		@SuppressWarnings("serial")
+		JavaRDD<String> filteredLines = lines.filter(
+				new Function<String, Boolean>() {
+					public Boolean call(String x) {
+						return x.contains("error");
+					}
+				});
+		System.out.println("--- Filtered lines:");
+		for(String line: filteredLines.collect())
+				System.out.println(line);
+		System.out.println("----------------------");
+		System.out.println();
+		
+		System.out.println("----------------------");
+		System.out.println("--- Map");
+		JavaRDD<Integer> numberList = sc.parallelize(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
+		@SuppressWarnings("serial")
+		JavaRDD<Integer> mapFiltered = numberList.map(
+				new Function<Integer, Integer>() {
+					public Integer call(Integer x) {
+						return x * x;
+					}
+				});
+		for(Integer line: mapFiltered.collect())
+			System.out.println(line);		
+		System.out.println("----------------------");
+		System.out.println();
+		
+		System.out.println("----------------------");
+		System.out.println("--- FlatMap");
+		JavaRDD<String> stringList = sc.parallelize(Arrays.asList("Hola que tal", "que has echo hoy", "como lo llevas", "estamos muy listos", "no sale esto", "o puede que si"));
+		@SuppressWarnings("serial")
+		JavaRDD<String> flatMapList = stringList.flatMap(
+				new FlatMapFunction<String, String>() {
+					public Iterator<String> call(String line) {
+						return Arrays.asList(line.split(" ")).iterator();
+					}
+				});
+		for(String line: flatMapList.collect())
+			System.out.println(line);
+		System.out.println("----------------------");
+		System.out.println();
+		
+		System.out.println("----------------------");
+		System.out.println("--- Distinct");
+		JavaRDD<String> anotherStringList = sc.parallelize(Arrays.asList("Hola", "Hola", "Adios", "Adios", "Adios", "Bye"));
+		JavaRDD<String> distinctList = anotherStringList.distinct();
+		System.out.println("--- Original List");
+		for(String line: anotherStringList.collect())
+			System.out.println(line);
+		System.out.println("--- Distinct List");
+		for(String line: distinctList.collect())
+			System.out.println(line);
+		System.out.println("----------------------");
+		System.out.println();
+		
+		System.out.println("----------------------");
+		System.out.println("--- Sample");
+		JavaRDD<String> sampleRDD = anotherStringList.sample(false, 0.5);
+		for(String line: sampleRDD.collect())
+			System.out.println(line);
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("   TWO RDDs transformations");
+		System.out.println();
+		JavaRDD<Integer> firstRDD = sc.parallelize(Arrays.asList(1,2,3));
+		JavaRDD<Integer> secondRDD = sc.parallelize(Arrays.asList(3,4,5));
+		System.out.println("--- First RDD");
+		for(Integer line: firstRDD.collect())
+			System.out.println(line);		
+		System.out.println("--- Second RDD");
+		for(Integer line: secondRDD.collect())
+			System.out.println(line);		
+		System.out.println("----------------------");
+		System.out.println("--- Union");
+
+		JavaRDD<Integer> unionRDD = firstRDD.union(secondRDD);
+		System.out.println("--- Union RDD");
+		for(Integer line: unionRDD.collect())
+			System.out.println(line);		
+		
+		
+		System.out.println("----------------------");
+		System.out.println("--- Sample");
+		System.out.println("----------------------");
+		System.out.println("--- Sample");
+		System.out.println("----------------------");
+		System.out.println("--- Sample");
+		
+		sc.close();
+	}
+}
